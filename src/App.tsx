1caa6a52627ff4b29e5ca2bf82853ea9980f2039
ousama <ousama.hassan@epita.fr>
2025-10-06 11:07:42 +0200

    5 parts done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import CardComp from "./components/CardComp"
+import cards from "./data/cards.json"
+import type { TCard, TCardList } from "./types/card.types"
+import { useState } from "react"
+
 
 const App = () => {
-	return (
-		<div>
-			<h1>Memory Game</h1>
-			<CardComp />
-		</div>
+
+  const createGameCards = (): TCardList => {
+	const pairs = cards.flatMap((card) => [
+		{ ...card, id: card.id },
+		{ ...card, id: card.id + 100 },
+		])
+	return pairs
+	}
+  // logging the data to the console
+  const [gameCards, setGameCards] = useState<TCardList>(cards)
+  console.log(gameCards)
+
+  return (
+  	<div className="main_section">
+  	  <h1>Memory Game</h1>
+  	  <div className="card_container">
+  	    {gameCards.map((card: TCard) => {
+  	      return <CardComp card={card} clickProp={() => {}} key={card.id} />
+  	    })}
+  	  </div>
+  	</div>
 	)
 }
 
